1ad9ac270567d7e3be6620911ed29582169aaa34
lxacha <lxacha@proton.me>
2026-05-17 17:02:36 +0200

    adding c folder containing c exercice

---
+++ c/learn1/adder.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+
+int main(int a,int b,int c) {
+	
+	int result = a + b + c;
+	printf("this is %i\n",result);
+	return result;
+}
---
+++ c/learn1/helloWorld.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+	int answer = 42;
+	printf("Hello World %i\n",answer);
+}
