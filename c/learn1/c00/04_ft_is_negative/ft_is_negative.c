ac641770fc8fc3665f04b3842470218fa53a0fe5
xaxa <xaxa@xaxa-t60.taile9468f.ts.net>
2026-05-23 10:34:55 +0200

    completed exercice 4, started 5.

--- c/learn1/c00/04_ft_is_negative/ft_is_negative.c
+++ c/learn1/c00/04_ft_is_negative/ft_is_negative.c
@@ -1,9 +1,29 @@
 #include <stdio.h>
-#include <unisd.h>
+#include <unistd.h>
+#include <stdlib.h>
 
-
-void ft_is_negative(int n);
+void ft_is_negative(int n)
 {
+	if (n == 0){
+		write(1,"null",4);
+	}
+	else if(n < 0){
+		write(1,"neg",3);
+	}
+	else{
+		write(1,"pos",3); 
+	}
+}
 
 
+
+
+
+
+int main(int argc, char *argv[])
+{
+	int a = atoi(argv[1]);
+
+	ft_is_negative(a);
+	return 0;
 }
---
+++ c/learn1/c00/05_ft_print_comb/ft_print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.>
+
+
+void ft_print_comb(void)
+{
+
+
+
+}
+
+int main(int argc, char *argv[])
+{
+
+
+
+
+
+}
