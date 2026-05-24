86f550a714a425fa14827bd772375ee434d8c854
lxacha <lxacha@proton.me>
2026-05-24 16:17:57 +0200

    advances exercie 05

--- c/learn1/c00/05_ft_print_comb/ft_print_comb.c
+++ c/learn1/c00/05_ft_print_comb/ft_print_comb.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <stdlib.>
-
-
+#include <stdlib.h>
 void ft_print_comb(void)
 {
+        int *a = NULL;
+        int taille = 1;
+        int max = 1000;
 
-
-
+        for (int i=0; i < max; i++){
+	a = realloc(a,taille * sizeof(int));
+        a[i] = i;
+	taille++;
+        printf("%d\n",a[i]);
+        }
 }
-
-int main(int argc, char *argv[])
+int main(void)
 {
-
-
-
-
-
+	ft_print_comb();
+	return 0;
 }
