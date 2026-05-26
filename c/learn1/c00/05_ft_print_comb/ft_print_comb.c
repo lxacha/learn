c39bf3afdedb98382d1f195bbf66f121d7c7f882
lxacha <lxacha@proton.me>
2026-05-26 07:17:45 +0200

    getting closer to the end for that one heck yeah

--- c/learn1/c00/05_ft_print_comb/ft_print_comb.c
+++ c/learn1/c00/05_ft_print_comb/ft_print_comb.c
@@ -16,39 +16,41 @@ void ft_print_comb(void)
         	printf("%d\n",a[i]);
         }
 
-	int * ab= NULL;
-	int tailleab = 1;
+	int ab[999][3]= {0};		/*liste de 999 listes de 3 int */
 
-	for (int i=0; i < max; i++ ){
-		ab = realloc(ab,taille * sizeof(int));
-		int backa = a[i];
-		int  primesin[3] = {0,0,0};
-			while (a[i] > 9){
-				a[i] = a[i]/10;		
-		}
-		primesin[0] = prime[a[i]];
+	for (int i=0; i < max; i++ ) 	/* pour tant que il n'y a pas eu plus 999 repetition*/
+	{
+
+		int backa = a[i];	/* backup de la valeur de i dans la liste a*/
+		int  primesin[3] = {0}; /*liste de 3 int*/
+
+		a[i] = a[i] % 10;	/*combien de fois y a t'il 10 dans i de a (25 = 2, 300 = 30)*/
+			
+		primesin[2] = prime[a[i]];
 		a[i] = backa;   
-			while (a[i] > 9){
-				a[i] = a[i]/100;
-		}
+		a[i] = a[i]/10;
+		a[i] = a[i] % 10;
+
 		primesin[1] = prime[a[i]];
 		a[i] = backa;
-			while (a[i] > 9){
-				a[i] = a[i]/1000;
-		}
-		primesin[2] = prime[a[i]];
+		a[i] = a[i]/100;
+		a[i] = a[i] % 10;
+	
+		primesin[0] = prime[a[i]];
 		a[i] = backa;
 		
-		tailleab++;
-	printf("%d",ab[1]);
-	}
+		ab[i][0] = primesin[0];
+		ab[i][1] = primesin[1];
+		ab[i][2] = primesin[2];
 
 
-/*	for (int x=0;x < 3; x++){
-	
+	printf("%d,%d,%d\n",ab[i][0],ab[i][1],ab[i][2]);
 	}
-*/
+
 }
+
+
+
 int main(void)
 {
 	ft_print_comb();
