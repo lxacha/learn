9b6f626ce9982e7e39976beae732f2531601d9e7
lxacha <lxacha@proton.me>
2026-06-03 19:36:22 +0200

    minimal advancement , looking into write , pointers, atoi alternative

--- c/learn1/c00/07_ft_putnbr/ft_putnbr.c
+++ c/learn1/c00/07_ft_putnbr/ft_putnbr.c
@@ -1,15 +1,23 @@
-<#include stdio.h>
-<#include unistd.h>
-<#include stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 
 void ft_putnbr(int nb){
-
-
-
+	if(nb <= 2147283647){
+		printf("%d",nb);
+	        write(1,&nb,sizeof(int));
+	}
 }
 
+
+
+
 int main(int argc,char *argv[]){
-	ft_putnbr();
+	char  b = argv[1][0];
+	int a = b - '0';
+	//printf("%d",a);
+	ft_putnbr(a);
+	return 0;
 
 }
