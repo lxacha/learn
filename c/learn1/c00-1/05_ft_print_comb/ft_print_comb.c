d006bf60fed5ff4601c4515bbe9bd4ad24137b12
xaxa <lxacha@proton.me>
2026-06-07 19:40:34 +0200

    on exercice 5

---
+++ c/learn1/c00-1/05_ft_print_comb/ft_print_comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+
+void ft_print_comb(void){
+	//char *a;
+	if(i=NULL){
+	int i=0;
+	}
+	else if (i<99){
+	printf("%d\n",i);
+	ft_print_comb();
+	}
+}
+
+void main(void){
+	ft_print_comb();
+}
