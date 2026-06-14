4b19a22f6bd8cef885db557a3c595809b33e63bb
xaxa <lxacha@proton.me>
2026-06-14 13:42:17 +0200

    08

--- c/learn1/c00-1/08_print_combn/ft_print_combn.c
+++ c/learn1/c00-1/08_print_combn/ft_print_combn.c
@@ -5,35 +5,39 @@ void ft_print_combn(int n){ // n = 3
 	//printf("%d",n);
 	int i=0;
 	int y;
-	int nbdigi= 1;	
-	
-	
+	int nbdigi= 1;
+
 	while(i<n){
 	nbdigi = nbdigi *10;        // 10 , 100 , 1000 nbdigi
 	i++;
 	//printf("%d",nbdigi);
 	}
-	
+
 	y = 1;
 	while(y<nbdigi){
 	printf("%d\n",y);
 	check_ascend(y,n,nbdigi);       // y 0, 1, 2 ... 387.. 997,998,999   , n = 3
 	y++;
 	}
-	
-}  
+
+}
 
 int check_ascend(int yp,int nba,int nbdigi2){   // yp 789 ,  nba 3, nbdigi2 1000
-	static int d= nbas;         // nbas 3
-	int iter = 0;
-	char *ldig[];
-	
-	if(nbas != 0){
-	while(iter < nbas){      // 1 ..  2  .. 3
-		nbdigi2 = nbdigi2/ 10;
-		int appt =
+	int d= nba;         // nbas 3
+	static int iter = 0;
+	char *ldig[30];
+
+	if(nba != 0){
+	if(iter <= nba){      // 1 ..  2  .. 3
+		while(nbdigi2>yp){
+		nbdigi2 = nbdigi2 /10;
+		}
+
+		int Aa = yp/nbdigi2;
 		iter++;
-		ldig[1][iter] = ;
+
+		ldig[1][iter] = Aa;
+		write(1,&ldig[1][iter != 0],1);
 	}
 	}
 
